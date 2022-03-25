b36ac76b1afb0794600268fc4127d717b7b2461e
Anant <Anant@LAPTOP-VMVFSFCA>
2022-03-25 15:46:11 +0530

    Pratical 2 task01

---
+++ JavaProgramming/src/Unit_01/P2_Task01_KeywordInJava.java
@@ -0,0 +1,38 @@
+package Unit_01;
+
+public class P2_Task01_KeywordInJava {
+
+	public static void main(String[] args) {
+		//variable Name
+		//Syntax error on token "double", invalid VariableDeclaratorId;
+		int double=10;
+		
+		//Funtion Name
+		//Syntax error on token "new", invalid AnnotationName
+		int new()
+		{
+			System.out.println("Keyword in java ");
+			return 0;
+		}
+		
+		//object name
+		//Syntax error on token "int", delete this token
+		ABC int=new ABC();
+	}
+}	
+		
+		
+class int{
+	void display()
+	{
+		System.out.println("Another class ..");
+	}
+}
+		
+ class ABC
+ {
+	 void main(string args)
+	 {
+		 System.out.println("Another class.......");
+	 }
+ }

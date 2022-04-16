fa4adfe0244b11b43800a6efe5a593f556e9bc2b
Anant <Anant@LAPTOP-VMVFSFCA>
2022-04-16 21:41:42 +0530

    Question based on array and string

---
+++ JavaProgramming/src/Unit_01/P8_String.java
@@ -0,0 +1,86 @@
+package Unit_01;
+
+/* Problem Statement:
+P8_String: 
+(Create separate logic in separate function)
+F1 - Check the entered string is palindrome or not? 
+  		String s = "75457"
+  		Output: Yes it is a palindrome or No it is not a palindrome.
+F2 - Make a reverse of a string using?
+F3 - String Compare: Check if the strings are equal or not?
+ * */
+
+public class P8_String {
+
+	public static void main(String[] args) {
+
+		QuestionsOnString obj = new QuestionsOnString();
+
+		String s1 = new String("75457");
+		String s2 = new String("7545a");
+
+		obj.palindromeOrNot(s1);
+		obj.reverseOfAString(s2);
+		obj.stringEqualOrNot(s1, s2);
+
+	}
+
+}
+
+class QuestionsOnString {
+
+	void palindromeOrNot(String s) {
+		// Write Logic Here! 
+		int l=s.length()-1;
+		int i=0;
+		while(i<l)
+		{
+			if(s.charAt(i)==s.charAt(l))
+			{
+				l--;
+				i++;
+				continue;
+			}
+			else
+			{
+				break;
+			}
+		}
+		if(i<l)
+		{
+			System.out.println("String is Not Palindrome");
+		}
+		else
+		{
+			System.out.println("String is Palindrome");
+		}
+	}
+
+	void reverseOfAString(String s) {
+		// Write Logic Here!
+		int l=s.length()-1;
+		String str="";
+		while(l>=0)
+		{
+			str=str+s.charAt(l);
+			l--;
+		}
+		s=str;
+		System.out.print("String after reversing: ");
+		System.out.println(s);
+
+	}
+	
+	void stringEqualOrNot(String s1,String s2) {
+		// Write Logic Here!
+		if(s1==s2)
+		{
+			System.out.println("Strings are equal");
+		}
+		else
+		{
+			System.out.println("Strings are not equal");
+		}
+		
+	}
+}
---
+++ JavaProgramming/src/Unit_01/P9_Array.java
@@ -0,0 +1,125 @@
+package Unit_01;
+
+/* Tasks given below:
+		obj.sortAnArray(array);
+		obj.findTheDuplicateElements(array);
+		obj.findSecondLargestAndSecondSmallestElement(array);
+		obj.leftRotationInAnArray(array); 
+		obj.removeElementInArray(array);//(Optional)
+ */
+
+public class P9_Array {
+
+	public static void main(String[] args) {
+
+		int[] array = { 4, 2, 3, 1, 6, 4, 3, 5, 2 };
+
+		QuestionsOnArray obj = new QuestionsOnArray();
+		obj.sortAnArray(array);
+		obj.findTheDuplicateElements(array);
+		obj.findSecondLargestAndSecondSmallestElement(array);
+		obj.leftRotationInAnArray(array);
+		obj.removeElementInArray(array); // (Optional)
+
+	}
+
+}
+
+class QuestionsOnArray {
+
+	void sortAnArray(int[] arr) {
+		int size=arr.length;
+		for(int i=0;i<size-1;i++)
+		{
+			int mn=i;
+			for(int j=i+1;j<size;j++)
+			{
+				if(arr[j]<arr[mn])
+				{
+					mn=j;
+				}
+			}
+			if(mn!=i)
+			{
+				int temp=arr[i];
+				arr[i]=arr[mn];
+				arr[mn]=temp;
+			}
+		}
+		System.out.print("Aarray after sorting:");
+		for(int i=0;i<size;i++)
+		{
+			System.out.print(arr[i]);
+			System.out.print(" ");
+		}
+
+	}
+
+	void findTheDuplicateElements(int[] arr) {
+		// write code here!
+		System.out.print("\nDuplicates element in the array are:");
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=i+1;j<arr.length;i++)
+			{
+				if(arr[i]==arr[j])
+				{
+					System.out.print(arr[i]);
+					System.out.print(" ");
+					break;
+				}
+			}
+		}
+
+	}
+
+	void findSecondLargestAndSecondSmallestElement(int[] arr) {
+		// write code here! 
+		int l1=arr[0],l2=arr[0];
+		for(int i=1;i<arr.length;i++) {
+			if(arr[i]>l1)
+			{   
+				l2=l1;
+				l1=arr[i];
+			}
+			else
+			{
+				if(arr[i]>l2)
+				{
+					l2=arr[i];
+				}
+			}
+			
+		}
+		System.out.print("\nLargest element in the array: ");
+		System.out.println(l1);
+
+		System.out.print("Second Largest element in the array: ");
+		System.out.println(l2);
+
+	}
+
+	void leftRotationInAnArray(int[] arr) {
+		// write code here! 
+		int temp=arr[0];
+		for(int i=1;i<arr.length;i++)
+		{
+			arr[i-1]=arr[i];
+		}
+		arr[arr.length-1]=temp;
+		System.out.print("Aarray after left rotating the array: ");
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.print(arr[i]);
+			System.out.print(" ");
+		}
+
+	}
+
+	void removeElementInArray(int[] arr) {
+		// write code here! 
+		
+
+	}
+
+}

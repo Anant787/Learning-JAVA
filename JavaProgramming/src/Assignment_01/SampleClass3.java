47d57553896c41aa9ceca5e51258450aa83582e9
Anant <Anant@LAPTOP-VMVFSFCA>
2022-05-07 21:38:19 +0530

    Sample class 3

---
+++ JavaProgramming/src/Assignment_01/SampleClass3.java
@@ -0,0 +1,18 @@
+package Assignment_01;
+
+public class SampleClass3 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
+class test3
+{
+	int a=10;
+	public int b=30;
+	private int c=23;
+	protected int d=90;
+	
+}
